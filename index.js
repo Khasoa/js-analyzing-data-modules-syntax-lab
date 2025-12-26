9c0d0882179bdd286e78551057ed6b43cf67f586
Lydia Khasoa <khasoalydia@gmail.com>
2025-12-26 22:31:45 +0300

    finish analyzing data with modules and syntax

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
+//datejs is a tool required once, outside the function
+require('datejs')
 
+//Create a function that merges all usernames into one.
+function combineUsers(...args) {
+  const combinedObject = {
+  users: []
+  }
 
+  for(let i = 0; i < args.length; i++) {
+    combinedObject.users.push(...args[i])
+  }
+
+  combinedObject.merge_date = Date.today().toString("M/d/yyyy")
+
+  return combinedObject
+}
 
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
